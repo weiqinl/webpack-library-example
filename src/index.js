1eb28b146f6a33fba38555d37be3e7b3989a4933
weiqinl <772863869@qq.com>
2018-10-14 22:54:13 +0800

    A: 增加注释

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import numRef from './ref.json'
 import packageConfig from '../package.json'
-
 const numberWord = () => {
+  // 数字转单词
   const numToWord = (num) => {
     let returnValue = _.reduce(numRef, (accum, ref) => {
       return ref.num === num ? ref.word : accum
     }, '')
     return returnValue === '' ? '请输入0-10中的某个整数' : returnValue
   }
-
+  // 单词转数字
   const wordToNum = (word) => {
     let resultValue = _.reduce(numRef, (accum, ref) => {
       return ref.word === word || word.toLowerCase() === ref.word.toLowerCase() ? ref.num : accum
@@ -17,7 +17,7 @@ const numberWord = () => {
   }
 
   return {
-    version: packageConfig.version,
+    version: packageConfig.version, // 版本号
     numToWord,
     wordToNum
   }
